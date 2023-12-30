3cfb7e7ae043282b6a3d08ef3035e86d638bfc81
Mohamed Mafaz <mohdmafaz200303@gmail.com>
2023-12-30 23:44:11 +0530

    Added comments. issue #17

--- html_extractor.py
+++ html_extractor.py
@@ -1,39 +1,50 @@
 import requests
 from bs4 import BeautifulSoup
 
-def get_html(urls : list[str]):
+def get_html(urls: list[str]) -> (dict[str, str], int):
     """
-    Function that takes in urls and returns the html content within it
+    Function that takes in URLs and returns the HTML content within them.
 
     Args:
-    urls = List of all urls
+    urls: List of all URLs.
 
-    Return:
-    Dictionary: {urls : html content}
-    failed_fetch: websites that it coudl'nt access during fetching html
+    Returns:
+    website_content: Dictionary {url: html_content}.
+    failed_fetch: Number of websites that couldn't be accessed during fetching HTML.
     """
-    website_content = {}
-    failed_fetch = 0
+    website_content = {}  # Dictionary to store HTML content for each URL
+    failed_fetch = 0  # Counter for failed fetch attempts
 
+    # Iterate through each URL in the list
     for website_idx in range(len(urls)):
         website = urls[website_idx]
+
+        # Send an HTTP GET request to the website
         response = requests.get(website)
 
+        # Check if the request was successful (status code 200)
         if response.status_code == 200:
+            # Parse the HTML content using BeautifulSoup
             soup = BeautifulSoup(response.content, 'html.parser')
             text = soup.get_text()
         else:
+            # If the request was not successful, increment the failed_fetch counter
             failed_fetch += 1
 
+        # Split text into lines, filter out empty lines, and join the non-empty lines
         lines = text.splitlines()
         non_empty_lines = [line for line in lines if line.strip()]
         result = '\n'.join(non_empty_lines)
+
+        # Store the result in the dictionary with the URL as the key
         website_content[website] = result
 
     return website_content, failed_fetch
 
-## Usecase:
+# Usecase example:
 """
 text2, failed_fetch_html = get_html(["https://timesofindia.indiatimes.com/india/whole-world-waiting-for-22nd-january-pm-modi-in-ayodhya/articleshow/106388185.cms"])
 print(text2)
 """
+
+# Note: Uncomment the usecase example when using this code.
